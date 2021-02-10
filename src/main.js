82775591e812ebe6462f6642e595571b8dcf025c
ahmedamged <ahmedamged@live.com>
2021-02-10 10:43:05 +0200

    Removing router configurations

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-// import axios from 'axios'
-// import VueAxios from 'vue-axios'
-import router from './router'
+// import router from './router'
 
-// createApp(App).use(VueAxios, axios).mount('#app')
-createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
+// createApp(App).use(router).mount('#app')
